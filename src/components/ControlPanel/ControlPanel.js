ff2c316ae6302c5186c12b10b5c79d31699aab0f
dongerina <donnasoujeh@gmail.com>
2018-06-29 02:44:07 +0200

    sorted heroes and added sections for herogrid

--- src/components/ControlPanel/ControlPanel.js
+++ src/components/ControlPanel/ControlPanel.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import axios from 'axios';
+import classes from './ControlPanel.css';
 
 class ControlPanel extends Component
 {
@@ -11,6 +12,18 @@ class ControlPanel extends Component
  {
   axios.get("https://api.opendota.com/api/heroStats")
   .then(response => {
+      console.log(response.data)
+      var sortedData = response.data.sort((a,b) => {
+        if (a.localized_name < b.localized_name)
+        {
+         return -1;
+        }
+        if (a.localized_name > b.localized_name)
+        {  
+         return 1;
+        }
+        return 0;
+      })
      this.setState({fetchedHeroes: response.data})
   }).catch(error =>{
       console.log(error)
@@ -19,14 +32,34 @@ class ControlPanel extends Component
  }
 
 
+
  render()
  {
-  let images = this.state.fetchedHeroes.map(image => <img key={image.id} src={'http://cdn.dota2.com/'+ image.img}/>)
+    
+  const imagesStr = this.state.fetchedHeroes.map(image =>{  
+      if(image.primary_attr === 'str')
+      {return (<img className={classes.hero} key={image.id} src={'http://cdn.dota2.com/'+ image.img}/>)}})
+
+  const imagesAgi = this.state.fetchedHeroes.map(image =>{  
+        if(image.primary_attr === 'agi')
+        {return (<img className={classes.hero} key={image.id} src={'http://cdn.dota2.com/'+ image.img}/>)}})
+        
+  const imagesInt = this.state.fetchedHeroes.map(image =>{  
+            if(image.primary_attr === 'int')
+            {return (<img className={classes.hero} key={image.id} src={'http://cdn.dota2.com/'+ image.img}/>)}})
+   
    return (<div>
-    <p>Strength</p>
-    {images}
-    <p>Agility</p>
-    <p>Intelligence</p>
+    <div className={classes.strength}><p>Strength</p>
+    {imagesStr}
+    </div>
+    <br/>
+    <div className={classes.agility}><p>Agility</p>
+    {imagesAgi}
+    </div>
+    <br/>
+    <div className={classes.intelligence}><p>Intelligence</p>
+    {imagesInt}
+    </div>
     </div>
     );
  }
